19f33c6448d9b0a84a3920bafc84bf9d4d1ca61f
liangz98 <liangz98@qq.com>
2018-04-18 02:18:49 +0800

    modified: 测试日志读取

--- application/app4center/default/controllers/LogController.php
+++ application/app4center/default/controllers/LogController.php
@@ -109,7 +109,7 @@ class LogController extends Seed_Controller_Action4Admin {
                     $files[$index]['id'] = $index + 1;
                     $files[$index]['name'] = $value;
                     $files[$index]['fileTime'] = date('Y-m-d H:i:s', filectime($directory . $value));
-                    $files[$index]['fileSize'] = $this->changeFileSize(filesize($directory . $value), 'kb').' kb';
+                    $files[$index]['fileSize'] = $this->size(filesize($directory . $value));
                     $files[$index]['page'] = $page;
                     $files[$index]['limit'] = $limit;
                 }
@@ -119,19 +119,6 @@ class LogController extends Seed_Controller_Action4Admin {
         $result['data'] = $files;
         echo json_encode($result);
         exit;
-    }
-    
-    function changeFileSize($size, $format) {
-        $p = 0;
-        if ($format == 'kb') {
-            $p = 1;
-        } elseif ($format == 'mb') {
-            $p = 2;
-        } elseif ($format == 'gb') {
-            $p = 3;
-        }
-        $size /= pow(1024, $p);
-        return number_format($size);
     }
     
     function loginfoAction() {
@@ -150,5 +137,53 @@ class LogController extends Seed_Controller_Action4Admin {
     
         echo $fileContent;
         exit;
+    }
+    
+    function changeFileSize($size, $format) {
+        $p = 0;
+        if ($format == 'kb') {
+            $p = 1;
+        } elseif ($format == 'mb') {
+            $p = 2;
+        } elseif ($format == 'gb') {
+            $p = 3;
+        }
+        $size /= pow(1024, $p);
+        return number_format($size);
+    }
+    
+    function size($byte) {
+        if ($byte < 1024) {
+            $unit = "B";
+        } else if ($byte < 10240) {
+            $byte = $this->round_dp($byte / 1024, 2);
+            $unit = "KB";
+        } else if ($byte < 102400) {
+            $byte = $this->round_dp($byte / 1024, 2);
+            $unit = "KB";
+        } else if ($byte < 1048576) {
+            $byte = $this->round_dp($byte / 1024, 2);
+            $unit = "KB";
+        } else if ($byte < 10485760) {
+            $byte = $this->round_dp($byte / 1048576, 2);
+            $unit = "MB";
+        } else if ($byte < 104857600) {
+            $byte = $this->round_dp($byte / 1048576, 2);
+            $unit = "MB";
+        } else if ($byte < 1073741824) {
+            $byte = $this->round_dp($byte / 1048576, 2);
+            $unit = "MB";
+        } else {
+            $byte = $this->round_dp($byte / 1073741824, 2);
+            $unit = "GB";
+        }
+    
+        $byte .= ' '.$unit;
+        return $byte;
+    }
+    
+    function round_dp($num, $dp) {
+        $sh = pow(10, $dp);
+        return (round($num * $sh) / $sh);
     }
 }
