af65d20e0e453494ee1d0e88fcf6cca0530d433c
liangz98 <liangz98@qq.com>
2018-04-17 17:58:38 +0800

    modified: 测试日志读取

--- application/app4center/default/controllers/LogController.php
+++ application/app4center/default/controllers/LogController.php
@@ -34,7 +34,8 @@ class LogController extends Seed_Controller_Action4Admin {
         /* 读取本地日志文件 */
     
         // $filePath = $_SERVER["DOCUMENT_ROOT"] . "/checkdata/app/files/nlp.txt";
-        $filePath = "/storage/html/eTradeFastWebhooksTest/eTradeFastPhp/public/ky/mod2018-04-17-16.log.txt";
+        // $filePath = "/storage/html/eTradeFastWebhooksTest/eTradeFastPhp/public/ky/mod2018-04-17-16.log.txt";
+        $filePath = "index.phtml";
         $file = fopen($filePath, "r");
     
         // $str = file_get_contents('/ky/mod2018-04-17-16.log.txt');//将整个文件内容读入到一个字符串中
