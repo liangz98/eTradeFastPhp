237d13201e687da8eb2886642ef30b422ea71897
liangz98 <liangz98@qq.com>
2018-04-18 14:36:45 +0800

    modified: 开票资料

--- application/app4www/user/views/helpers/ShowTopContract.php
+++ application/app4www/user/views/helpers/ShowTopContract.php
@@ -3,24 +3,24 @@
 class Zend_View_Helper_ShowTopContract extends Shop_View_Helper {
     
     //数组转对象
-    public function arrayToObject($e){
-        
-        if( gettype($e)!='array' )
-            return;
-        foreach($e as $k=>$v){
-            if( gettype($v)=='array' || getType($v)=='object' )
-                $e[$k]=(object)arrayToObject($v);
+    public function arrayToObject($e) {
+        if (gettype($e) != 'array')
+            return false;
+        foreach ($e as $k => $v) {
+            if (gettype($v) == 'array' || getType($v) == 'object')
+                $e[$k] = (object)arrayToObject($v);
         }
         return (object)$e;
     }
     
     //对象转数组
-    public function objectToArray($e){
-        $e=(array)$e;
-        foreach($e as $k=>$v){
-            if( gettype($v)=='resource' ) return;
-            if( gettype($v)=='object' || gettype($v)=='array' )
-                $e[$k]=(array)$this->objectToArray($v);
+    public function objectToArray($e) {
+        $e = (array)$e;
+        foreach ($e as $k => $v) {
+            if (gettype($v) == 'resource')
+                return false;
+            if (gettype($v) == 'object' || gettype($v) == 'array')
+                $e[$k] = (array)$this->objectToArray($v);
         }
         return $e;
     }
@@ -37,65 +37,35 @@ class Zend_View_Helper_ShowTopContract extends Shop_View_Helper {
         $hasNoEContract = "False";
 
         foreach ($Arr as $k => $v) {
-            // var_dump($v->attachmentList); exit;
             if (is_object($v)) {
-                // objectToArray($v);
                 $v = $this->objectToArray($v);
             }
-            
-            // echo "in".is_object($v); exit;
-    
-            // if (is_object($valueObject)) {
-            //     $attachmentList = $valueObject->attachmentList;
-            // } else if (is_array($valueObject)) {
-            //     $attachmentList = $valueObject["attachmentList"];
-            // }
-            
-            // var_dump($valueObject); exit;
             
             if (count($v['attachmentList']) > 0) {
                 $attachmentList = $v['attachmentList'];
                 $pdfUrl = $this->view->seed_Setting['KyUrlex'] . '/doc/download.action?sid=' . session_id() . '&nid=' . $attachmentList[0]['attachID'] . '&vid=' . $attachmentList[0]['verifyID'];
     
-                //if(!empty($client)&&$accountID!=$client){continue;}
                 $IMG .= '<li style="width: 100%;float: left;margin: 10px 0;"><em class="new_upicon" style="background:url(\'/ky/ico/pdf.png\') no-repeat;background-size: 20px;"></em>';
                 $IMG .= '<p class="new_uptitle">' . $v['contractName'] . '</p>';
-                // $IMG .= '<div class="new_xian"><p class="new_desc">' . $v['description '] . '</p></div>';
                 $IMG .= '<input type="hidden" value="' . $v['attachType'] . '" />';
-    
-    
-                // $IMG .= '<a href="';
-                // $IMG .= $this->view->seed_Setting['KyUrlex'].'/doc/download.action?sid='.session_id().'&nid='.$attachmentList[0]['attachID'];
-                // $IMG .='&vid='.$attachmentList[0]['verifyID'].'" download>下载</a></span>';
     
                 $IMG .= '<input type="hidden" id="contractAttachUrl_'.$v['contractID'].'" value="' . $pdfUrl . '">';
                 $IMG .= '<input type="hidden" id="contractID_'.$v['contractID'].'" value="'.$v['contractID'].'" />';
                 $IMG .= '<input type="hidden" id="contractName_'.$v['contractID'].'" value="'.$v['contractName'].'" />';
                 $IMG .= '<input type="hidden" id="ext_'.$v['contractID'].'" value="'.$v['ext'].'" />';
     
-                if (!$v['isDeleted'] && !$v['isCancel'] && !$v['isClosed']) {
-                    if (($v['firstParty'] != null && $v['firstParty'] == $accountID && $v['firstPartySigningDate'] == null) || ($v['secondParty'] != null && $v['secondParty'] == $accountID && $v['secondPartySigningDate'] == null) || ($v['thirdParty'] != null && $v['thirdParty'] == $accountID && $v['thirdPartySigningDate'] == null)) {
-                        if (!$hasIDCertificate) {
-                            $IMG .= '<a href="javascript:void(0)" onclick="contractSignViewNO(\'' . $v['contractID'] . '\')" class="order_contract_sign fr">签署</a>';
-                        } else {
-                            if ($v['isEContract']) {
-                                $IMG .= '<a href="javascript:void(0)" id="' . $v['contractID'] . '" onclick="initPdfView(\'' . $pdfUrl . '\', this)" class="order_contract_sign fr">签署</a>';
-                            } else {
-                                $hasNoEContract = "True";
-                                $IMG .= '<a href="javascript:void(0)" id="' . $v['contractID'] . '" onclick="initSignViewNoEContract(this)" class="order_contract_sign fr">下载</a>';
-                            }
-                        }
+    
+                if ($v['isEContract']) {
+                    if (!$hasIDCertificate) {
+                        $IMG .= '<a href="javascript:void(0)" onclick="contractSignViewNO(\'' . $v['contractID'] . '\')" class="order_contract_sign fr">签署</a>';
+                    } else {
+                        $IMG .= '<a href="javascript:void(0)" id="' . $v['contractID'] . '" onclick="initPdfView(\'' . $pdfUrl . '\', this)" class="order_contract_sign fr">签署</a>';
                     }
-                    if (!$v['isDeleted'] && !$v['isCancel'] && !$v['isClosed']) {
-                        if (($v['firstParty'] != null && $v['firstParty'] == $accountID && $v['firstPartySigningDate'] != null) || ($v['secondParty'] != null && $v['secondParty'] == $accountID && $v['secondPartySigningDate'] != null) || ($v['thirdParty'] != null && $v['thirdParty'] == $accountID && $v['thirdPartySigningDate'] != null)) {
-                
-                            $IMG .= '<input type="hidden" id="isSign_' . $v['contractID'] . '" value="true">';
-                            $IMG .= '<a href="javascript:void(0)" id="' . $v['contractID'] . '" onclick="initPdfView(\'' . $pdfUrl . '\', this)" class="order_contract_sign fr" style="background: #ccc;">已签署</a>';
-                        }
-                    }
-                    $IMG .= '</li>';
-        
+                } else {
+                    $hasNoEContract = "True";
+                    $IMG .= '<a href="javascript:void(0)" id="' . $v['contractID'] . '" onclick="initSignViewNoEContract(this)" class="order_contract_sign fr">下载</a>';
                 }
+                $IMG .= '</li>';
             } else {
                 $IMG .= '<label>暂无数据！</label>';
             }
