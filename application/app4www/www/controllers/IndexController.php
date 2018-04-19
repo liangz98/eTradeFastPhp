a013760c1262a655bef342debe04d5f102866fab
liangz98 <liangz98@qq.com>
2018-04-19 16:22:36 +0800

    modified: 个人网签

--- application/app4www/www/controllers/IndexController.php
+++ application/app4www/www/controllers/IndexController.php
@@ -1,14 +1,13 @@
 <?php
-class IndexController extends Kyapi_Controller_Action
-{
 
-    public function indexAction()
-	{	
-
-		if(defined('SEED_WWW_TPL')){
-			$content = $this->view->render(SEED_WWW_TPL."/index/index.phtml");
-			echo $content;
-			exit;
-		}
-	}
+class IndexController extends Kyapi_Controller_Action {
+    
+    public function indexAction() {
+        
+        if (defined('SEED_WWW_TPL')) {
+            $content = $this->view->render(SEED_WWW_TPL . "/index/index.phtml");
+            echo $content;
+            exit;
+        }
+    }
 }
