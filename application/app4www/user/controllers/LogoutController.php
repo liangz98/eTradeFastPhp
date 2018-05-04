55e3b2687b21a4f5bcae20f06808aeb8925cec4a
liangz98 <liangz98@qq.com>
2018-05-04 15:06:09 +0800

    modified: 调整登出重定向路径

--- application/app4www/user/controllers/LogoutController.php
+++ application/app4www/user/controllers/LogoutController.php
@@ -10,17 +10,18 @@ class LogoutController extends Kyapi_Controller_Action {
         $redis->connect($config);
         $redis->delete('PHPREDIS_ACTIVE_USERS:' . $this->view->userID);
         $redis->delete('PHPREDIS_ACTIVE_SESSION:' . session_id());
-        
+
         //销毁session
         session_unset(session_id());
         session_destroy();
-        
+
         //设置cookie
         $expiretime = -1;
         Seed_Cookie::setCookie('PHPSESSID', '', $expiretime, $this->view->seed_Setting['cookie_path'], $this->view->seed_Setting['cookie_host']);
         $ajax = trim($this->_request->getParam('ajax'));
-        
-        $this->redirect("/");
+
+
+        $this->redirect($_SERVER['HTTP_REFERER']);
         exit;
     }
 }
