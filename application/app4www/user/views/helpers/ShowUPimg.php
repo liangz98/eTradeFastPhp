4e002eda8f7d9db20dd23cff37c8e44b93014eda
liangz98 <liangz98@qq.com>
2018-12-19 17:27:22 +0800

    modified: 订单,跟踪日志完善

--- application/app4www/user/views/helpers/ShowUPimg.php
+++ application/app4www/user/views/helpers/ShowUPimg.php
@@ -1,22 +1,67 @@
 <?php
-class Zend_View_Helper_ShowUPimg extends Shop_View_Helper
-{
-	function ShowUPimg($attArr)
-	{
-		//$attArr 服务端返回附件信息数组；
-		$str="";
-		if(is_array($attArr) && count($attArr)>0){
-			foreach($attArr as $k=>$attlist){
-				$str.='<img class="table_img"  src='. $this->view->seed_Setting['KyUrlex'];
-				$str.='/doc/download.action?sid='.session_id();
-				$str.='&nid='.$attlist['attachID'];
-				$str.='&vid='.$attlist['verifyID'];
-				$str.='&size=MIDDLE name='.$attlist['name'].'alt='.$attlist['attachType'].'>';
-			}
-		}else{
-			$str='<img class="table_img" src="/ky/images/goods_tx.png">';
-		}
 
-		return $str;
-	}
+class Zend_View_Helper_ShowUPimg extends Shop_View_Helper {
+    function ShowUPimg($attArr) {
+        //$attArr 服务端返回附件信息数组；
+        $str = "";
+        if (is_array($attArr) && count($attArr) > 0) {
+            foreach ($attArr as $k => $attlist) {
+                $ext = "";
+                $attachID = "";
+                $verifyID = "";
+                $name = "";
+                $attachType_ = "";
+                // $size = "";
+
+                if (is_object($attlist)) {
+                    $ext = $attlist->ext;
+                    $attachID = $attlist->attachID;
+                    $attachType_ = $attlist->attachType;
+                    $verifyID = $attlist->verifyID;
+                    $name = $attlist->name;
+                    // $size = $attlist->size;
+                } else if (is_array($attlist)) {
+                    $ext = $attlist["ext"];
+                    $attachID = $attlist["attachID"];
+                    $attachType_ = $attlist["attachType"];
+                    $verifyID = $attlist["verifyID"];
+                    $name = $attlist["name"];
+                    // $size = $attlist["size"];
+                }
+
+                if ($ext == "DOCX" || $ext == "WPS") {
+                    $ext = "doc";
+                }
+                if ($ext == "XLSX") {
+                    $ext = "xls";
+                }
+                if ($ext == "PPTX") {
+                    $ext = "ppt";
+                }
+
+                $downloadURL = $this->view->seed_Setting['KyUrlex'] . '/doc/download.action';
+
+                $str .= '<li style="cursor: pointer;">';
+                if ($ext == "jpeg" || $ext == "png" || $ext == "jpg" || $ext == "gif" || $ext == "GIF" || $ext == "JPG" || $ext == "PNG" || $ext == "JPEG") {
+                    $str .= '<img width="125px" height="125px" ';
+                    $str .= 'data-original="' . $downloadURL . '?sid=' . session_id().'&nid=' . $attachID.'&vid=' . $verifyID.'" ';
+                    $str .= 'src="' . $downloadURL . '?sid=' . session_id(). '&nid=' . $attachID . '&vid=' . $verifyID . '&size=MIDDLE" ';
+                    $str .= 'name="' . $name . '" alt="' . $attachType_ . '" >';
+                } elseif ($ext == "doc" || $ext == "xls" || $ext == "ppt") {
+                    $str .= '<img width="125px" height="125px" ';
+                    $str .= 'src="/ky/ico/' . strtolower($ext) . '.png" ';
+                    $str .= 'name="' . $name . '" alt="' . $attachType_ . '" >';
+                } else {
+                    $str .= '<img width="125px" height="125px" ';
+                    $str .= 'src="/ky/ico/video.png" ';
+                    $str .= 'name="' . $name . '" alt="' . $attachType_ . '" >';
+                }
+                $str .= '</li>';
+            }
+        }
+
+        $resultImg = '<ul class="showImgView">' . $str . '</ul>';
+
+        return $resultImg;
+    }
 }
