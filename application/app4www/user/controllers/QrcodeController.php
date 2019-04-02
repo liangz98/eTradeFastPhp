f339d81e888108948b1f667f6dc19cbaa40c0127
liangz98 <liangz98@qq.com>
2019-04-02 18:16:40 +0800

    modified: sale view

--- application/app4www/user/controllers/QrcodeController.php
+++ application/app4www/user/controllers/QrcodeController.php
@@ -1,20 +1,28 @@
 <?php
-class QrcodeController extends Zend_Controller_Action
-{
-	function preDispatch()
-	{
-		$this->view->cur_pos = $this->_request->getParam('controller'); 
-	}
-	
-	public function indexAction()
-	{
-		$url = $this->_request->getParam('url');
-		if($url=="")$url="http://".$_SERVER['HTTP_HOST'].$_SERVER["REQUEST_URI"];
-		else $url = urldecode($url);
-		$level = "M";
-		$size = 8;
-		include SEED_LIB_ROOT."/Plugin/Phpqrcode.php";
-		QRcode::png($url , false , $level, $size, 2);
-		exit;
-	}
+
+class QrcodeController extends Zend_Controller_Action {
+    function preDispatch() {
+        $this->view->cur_pos = $this->_request->getParam('controller');
+    }
+
+    public function indexAction() {
+        $url = $this->_request->getParam('url');
+        if ($url == "")
+            $url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER["REQUEST_URI"]; else $url = urldecode($url);
+        $level = "M";
+        $size = 8;
+        include SEED_LIB_ROOT . "/Plugin/Phpqrcode.php";
+        QRcode::png($url, false, $level, $size, 2);
+        exit;
+    }
+
+    public function appAction() {
+        $appURL = "http://" . $_SERVER['HTTP_HOST'] . '/app/etradefast-release.apk';
+
+        $level = "M";
+        $size = 8;
+        include SEED_LIB_ROOT . "/Plugin/Phpqrcode.php";
+        QRcode::png($appURL, false, $level, $size, 2);
+        exit;
+    }
 }
